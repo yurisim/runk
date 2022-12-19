dbcbc61aba0fa15a04869bbb498a37d7b007a462
matthew-eichler <eichlermatt21@gmail.com>
2022-12-19 11:05:04 -0600

    added stepper to pr

--- apps/prunk/src/app/pr/pr.component.ts
+++ apps/prunk/src/app/pr/pr.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'runk-pr',
   templateUrl: './pr.component.html',
   styleUrls: ['./pr.component.scss'],
 })
-export class PrComponent {}
+export class PrComponent {
+  constructor(private _formBuilder: FormBuilder) {}
+  
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+}
