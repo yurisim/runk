c00abeeab7767fd3faaf6ef9b91a0e94cd2798bf
Yuri Sim <edaraks@gmail.com>
2022-12-19 10:11:19 -0600

    made the module boundaries a warning instead

--- apps/prunk-api/src/prisma/prisma.service.ts
+++ apps/prunk-api/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'node_modules/.prisma/client';
+import { PrismaClient } from '.prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
