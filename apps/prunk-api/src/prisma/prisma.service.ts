55700aea8db0e0793719ffab8658a88e2cb93c61
Yuri Sim <edaraks@gmail.com>
2022-12-19 10:05:29 -0600

    fixed lint error

--- apps/prunk-api/src/prisma/prisma.service.ts
+++ apps/prunk-api/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '.prisma/client';
+import { PrismaClient } from 'node_modules/.prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
