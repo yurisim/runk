5c683680b306415bb467bc59d8c6cd94b402ccca
Ronald <ronald.taite.2@gmail.com>
2023-01-03 14:25:18 -0600

    Added code to connecting client app to auth0

--- apps/prunk-api/src/main.ts
+++ apps/prunk-api/src/main.ts
@@ -8,12 +8,26 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import * as dotenv from 'dotenv'
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+    }),
+  );
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   Logger.log(`🚀 Application is running on: http://localhost:${port}/graphql`);
 }
 
