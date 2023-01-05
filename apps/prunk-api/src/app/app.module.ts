8a86f82b80d999430a975d4289d78061d52b7d61
Yuri Sim <edaraks@gmail.com>
2023-01-04 22:49:31 -0600

    initial graphQL code gen documents

--- apps/prunk-api/src/app/app.module.ts
+++ apps/prunk-api/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { PrismaModule } from '../prisma/prisma.module';
 import { OrgModule } from '../org/org.module';
 import { PersonModule } from '../person/person.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { PersonModule } from '../person/person.module';
       driver: ApolloDriver,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      autoSchemaFile: true,
+      // create the autoschema file in the root of the project
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      sortSchema: true,
     }),
     PrismaModule,
     OrgModule,
